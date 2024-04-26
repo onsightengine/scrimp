76297d01f51f74a016ee2de78a5963898bde48b2
Stephens Nunnally <stevinz@gmail.com>
2024-04-26 12:03:57 -0700

    Added Eslint support

--- src/Scrimp.js
+++ src/Scrimp.js
@@ -13,35 +13,29 @@ import { closeBrackets, autocompletion, closeBracketsKeymap, completionKeymap } 
 import { lintKeymap, linter, lintGutter } from '@codemirror/lint';
 
 // Language
-import { javascript } from '@codemirror/lang-javascript';
+import { javascript, esLint } from '@codemirror/lang-javascript';
 import { python } from '@codemirror/lang-python';
+
+// Linter
+import * as esb from 'eslint-linter-browserify';
 
 // Theme
 // - Editor: https://thememirror.net/create
 import { oneDark } from '@codemirror/theme-one-dark';
 import { themeSuey } from './theme/theme-suey.js';
 
-// // Linter (find errors)
-// import esprima from 'esprima'; // v4.0.1 https://github.com/jquery/esprima
-//
-// const esprimaLinter = linter(view => {
-//     const diagnostics = [];
-//     const code = view.state.doc.toString();
-//     try {
-//         esprima.parse(code, { tolerant: true, loc: true });
-//     } catch (error) {
-//         if (error instanceof SyntaxError) {
-//             const diagnostic = {
-//             from: view.state.doc.line(error.lineNumber - 1).from + error.column - 1,
-//             to: view.state.doc.line(error.lineNumber - 1).to,
-//             message: error.description,
-//             severity: 'error',
-//             };
-//             diagnostics.push(diagnostic);
-//         }
-//     }
-//     return diagnostics;
-// });
+// Linter Config
+const jsConfig = {
+    languageOptions: {
+		parserOptions: {
+			ecmaVersion: 2023,
+			sourceType: "module",
+		},
+	},
+	rules: {
+		// semi: [ "error", "never" ],
+	},
+};
 
 class Scrimp extends EditorView {
 
@@ -51,7 +45,6 @@ class Scrimp extends EditorView {
             highlightActiveLineGutter(),
             highlightSpecialChars(),
             foldGutter(),
-            lintGutter(),
             // drawSelection(), //NOTE: Don't want, allows browser to draw native selection color
             dropCursor(),
             EditorState.allowMultipleSelections.of(true),
@@ -95,6 +88,7 @@ class Scrimp extends EditorView {
         // Compartments
         this.historyCompartment = new Compartment();
         this.languageCompartment = new Compartment();
+        this.gutterCompartment = new Compartment();
         this.linterCompartment = new Compartment();
 
         // History
@@ -103,7 +97,19 @@ class Scrimp extends EditorView {
         // Language
         if (options.language === 'javascript') {
             extensions.push(this.languageCompartment.of(javascript()));
-            // if (options.linter) extensions.push(esprimaLinter);
+            if (options.linter) {
+                extensions.push(this.gutterCompartment.of(lintGutter()));
+                extensions.push(this.linterCompartment.of(linter(esLint(new esb.Linter(), jsConfig))));
+            }
+        } else if (options.language === 'python') {
+            extensions.push(this.languageCompartment.of(python()));
+            if (options.linter) {
+                extensions.push(this.gutterCompartment.of());
+                extensions.push(this.linterCompartment.of());
+            }
+        } else if (!options.linter) {
+            extensions.push(this.gutterCompartment.of());
+            extensions.push(this.linterCompartment.of());
         }
 
         // Theme
@@ -189,12 +195,20 @@ class Scrimp extends EditorView {
 
     /******************** LANGUAGE */
 
-    setLanguage(language = 'javascript') {
+    setLanguage(language = 'javascript', includeLinter = true) {
         this.dispatch({ effects: [ this.languageCompartment.reconfigure([]) ] });
         if (language === 'javascript') {
             this.dispatch({ effects: [ this.languageCompartment.reconfigure([ javascript() ]) ] });
+            if (includeLinter) {
+                this.dispatch({ effects: [ this.gutterCompartment.reconfigure([ lintGutter() ]) ] });
+                this.dispatch({ effects: [ this.linterCompartment.reconfigure([ linter(esLint(new esb.Linter(), jsConfig)) ]) ] });
+            }
         } else if (language === 'python') {
             this.dispatch({ effects: [ this.languageCompartment.reconfigure([ python() ]) ] });
+            if (includeLinter) {
+                this.dispatch({ effects: [ this.gutterCompartment.reconfigure([]) ] });
+                this.dispatch({ effects: [ this.linterCompartment.reconfigure([]) ] });
+            }
         }
     }
 
